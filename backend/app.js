fc8d8915733cc418c2f42374d59c21e6c3bebe4f
saksham-intraintech <112855015+sakshamxt@users.noreply.github.com>
2024-12-23 23:53:11 +0530

    user authentication backend

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const connectDB = require('./db/db.js');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes.js');
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller.js');
+const authMiddleware = require('../middlewares/auth.middleware.js');
 
 
 router.post('/register', [
@@ -16,5 +17,9 @@ router.post('/login', [
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ], userController.loginUser);
 
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile);
+
+router.get('/logout', authMiddleware.authUser, userController.logoutUser);
 
 module.exports = router;
